d9119ee47535cc41dcbce0d515e6cc60f6aa2e17
Bassim <bassimsadik@hotmail.com>
2017-03-14 03:32:49 +0000

    print each avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -5,12 +5,29 @@ var GITHUB_TOKEN = "08b1711fcfaea2bb1cdce05f61f6fbcd1e584cfc";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-  getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': "GitHub Avatar Downloader - Student Project"
+    }
+  };
+  request(options, (err, response, body) => {
+    if (err) {
+      console.log("WTF", err);
+      return cb(false);
+    }
+    if (response && response.statusCode !== 200) {
+      console.log("Response was not 200!", response.statusCode);
+      return false;
+    }
+    let data = JSON.parse(body);
+    cb(null, data);
+  });
 }
+  getRepoContributors("jquery", "jquery", function(err, result) {
 
-getRepoContributors();
+  const avatarUrls = result.map(item => item.avatar_url)
+  console.log(avatarUrls);
+});
+
 console.log('Welcome to the GitHub Avatar Downloader!');
