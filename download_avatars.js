bb9f9b6effe2cef551c83e1603f19dfec3c0b4d2
Bassim <bassimsadik@hotmail.com>
2017-03-14 18:23:31 +0000

    eslint checks

--- download_avatars.js
+++ download_avatars.js
@@ -6,7 +6,7 @@ var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var options = {
     url: requestURL,
     headers: {
@@ -30,23 +30,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 function avatarList(err, result) {
   result.forEach(function(item){
-    downloadImageByURL(item.avatar_url, "./avatars/"+item.login+".jpg");
-  })
+    downloadImageByURL(item.avatar_url, "./avatars/" + item.login + ".jpg");
+  });
 }
 
 function downloadImageByURL(url, filePath) {
-   request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
+  request.get(url)
+      .on('error', function (err) {
+        throw err;
+      })
+      .on('response', function (response) {
         if (response.statusCode === 200) {
-         console.log('Response Status Message: ', response.statusCode, response.headers['content-type'], '\n' + "Download completed.")
+          console.log('Response Status Message: ', response.statusCode, response.headers['content-type'], '\n' + "Download completed.");
         } else {
           console.log("Download failed", response.statusCode);
         }
-       })
-       .pipe(fs.createWriteStream(filePath));
+      })
+      .pipe(fs.createWriteStream(filePath));
 }
 
 if (repoOwner && repoName) {
